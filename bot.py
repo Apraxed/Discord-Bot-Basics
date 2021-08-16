ba2a7549dd2ddde8a07386a8255ecb3090795db8
Zbot <78899706+ZbotDyn0@users.noreply.github.com>
2021-08-15 23:45:39 -0700

    Update and rename main.py to bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-import config, commands_config
+import config
+import commands_config
 import os
 
 ccfg = commands_config
@@ -9,10 +10,15 @@ cfg = config
 client = commands.Bot(command_prefix = cfg.BOT_PREFIX)
 
 @client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f'you forgot this one very important part!!! do `{cfg.BOT_PREFIX}help` for more detail')
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You dont have permission to execute this command")
+
+@client.event
 async def on_ready():
     print(client.user.name, 'is online')
-    channel =   channel = client.get_channel(cfg.channel_id)
-    await channel.send('Bot online')
 
 # Test command
 @client.command()
@@ -25,6 +31,7 @@ async def ping(ctx):
     ping = int(round(client.latency, 3) * 1000)
     await ctx.send(f'{ctx.author.mention} {ping} ms.')
 
+# Embed command
 @client.command()
 async def embed(ctx):
     embed = discord.Embed(title = ccfg.Example_Embed_Title, description = ccfg.Example_Embed_Title, color = 0x5865f2) # blurple hex code
@@ -32,5 +39,10 @@ async def embed(ctx):
     embed.add_field(name = ccfg.Embed_Name_2, value = (ccfg.Embed_Text_2))
     await ctx.send(embed = embed)
 
+@client.command()
+@commands.has_any_role('') #enter roles that can use command in that and devide them with >,< Don't close ""
+async def special(ctx):
+  await ctx.send('ok a guy with special role!')
+
 # Makes the bot run
-client.run(os.environ['TOKEN'])
+client.run(os.environ['token'])
